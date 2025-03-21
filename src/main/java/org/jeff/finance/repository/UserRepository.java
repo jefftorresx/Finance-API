b68f354fbf361cd6d85bb2209976b924a7985728
Jeffrey Richardson <jeff.dsv@gmail.com>
2025-03-21 15:14:44 -0400

    Implementa UserRepository utilizando JpaRepository para persistência

---
+++ src/main/java/org/jeff/finance/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.jeff.finance.repository;
+
+import org.jeff.finance.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
