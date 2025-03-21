7b47cb1247d2fe69a846502f950ac95efa79d307
Jeffrey Richardson <jeff.dsv@gmail.com>
2025-03-21 14:57:09 -0400

    Primeiro commit: adicionando projeto gerado pelo Spring

---
+++ src/test/java/org/jeff/finance/FinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jeff.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
